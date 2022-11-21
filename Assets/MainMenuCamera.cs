8fabd735a780ad53a36bf1b8099059c82959d767
Type-32 <wilsonsu2008@hotmail.com>
2022-11-21 22:52:37 +0800

    Changed Main Menu

---
+++ Assets/MainMenuCamera.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCamera : MonoBehaviour
+{
+    [SerializeField] GameObject originPoint;
+    [SerializeField] GameObject loadoutPoint;
+    [SerializeField] GameObject multiplayerPoint;
+    [SerializeField] GameObject shopPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = originPoint.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MainMenuUIManager.instance.openedMainMenu)
+        {
+            transform.position = Vector3.Slerp(transform.position, originPoint.transform.position, Time.deltaTime * 5f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, originPoint.transform.rotation, Time.deltaTime * 5f);
+        }
+        else if (MainMenuUIManager.instance.openedMultiplayerMenu)
+        {
+            transform.position = Vector3.Slerp(transform.position, multiplayerPoint.transform.position, Time.deltaTime * 5f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, multiplayerPoint.transform.rotation, Time.deltaTime * 5f);
+        }
+        else if (MainMenuUIManager.instance.openedLoadoutSelectionMenu)
+        {
+            transform.position = Vector3.Slerp(transform.position, loadoutPoint.transform.position, Time.deltaTime * 5f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, loadoutPoint.transform.rotation, Time.deltaTime * 5f);
+        }
+        else if (MainMenuUIManager.instance.openedShopMenu)
+        {
+            transform.position = Vector3.Slerp(transform.position, shopPoint.transform.position, Time.deltaTime * 5f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, shopPoint.transform.rotation, Time.deltaTime * 5f);
+        }
+        else
+        {
+            transform.position = Vector3.Slerp(transform.position, originPoint.transform.position, Time.deltaTime * 5f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, originPoint.transform.rotation, Time.deltaTime * 5f);
+        }
+    }
+}
