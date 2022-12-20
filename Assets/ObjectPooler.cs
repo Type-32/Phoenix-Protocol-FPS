9262d0f34e3083e35883e54e66afbade1cd522a4
Type-32 <wilsonsu2008@hotmail.com>
2022-12-20 10:22:58 +0800

    Changed Equipment Effects and Object Pooler Code

--- Assets/ObjectPooler.cs
+++ Assets/ObjectPooler.cs
@@ -27,7 +27,7 @@ public class ObjectPooler : MonoBehaviour
             Queue<GameObject> objectPool = new();
             for (int i = 0; i < pool.size; i++)
             {
-                GameObject obj = Instantiate(pool.prefab);
+                GameObject obj = Instantiate(pool.prefab, transform);
                 obj.SetActive(false);
                 objectPool.Enqueue(obj);
             }
