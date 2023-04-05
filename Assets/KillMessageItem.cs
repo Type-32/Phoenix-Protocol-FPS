e18740927edb0031d0d9e2b4ef4225b46437a52a
Type-32 <87076491+Type-32@users.noreply.github.com>
2023-04-06 00:09:12 +0800

    .

--- Assets/KillMessageItem.cs
+++ Assets/KillMessageItem.cs
@@ -9,6 +9,10 @@ public class KillMessageItem : MonoBehaviour
     [SerializeField] Text killer;
     [SerializeField] Image icon;
     [SerializeField] Image iconBG;
+    void Start()
+    {
+        Destroy(gameObject, 15f);
+    }
     public void SetInfo(string killedName, string killerName, Sprite img)
     {
         killed.text = killedName;
