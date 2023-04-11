20ec04c7df6f77be76130cfc3df50a76e55b654b
Type-32 <wilsonsu2008@hotmail.com>
2023-04-11 18:10:39 +0800

    Formatting EquipmentHolder.cs

--- Assets/Scripts/EquipmentHolder.cs
+++ Assets/Scripts/EquipmentHolder.cs
@@ -28,78 +28,48 @@ public class EquipmentHolder : MonoBehaviourPunCallbacks
         if (!player.pv.IsMine)
         {
             for (int i = 0; i < 2; i++)
-            {
-                InstantiateWeapon(player.matchLoadoutManager.slotWeaponData[i], i);
-            }
+                InstantiateWeapon(player.playerManager.matchLoadoutManager.slotWeaponData[i], i);
             for (int i = 0; i < 2; i++)
-            {
-                InstantiateEquipment(player.matchLoadoutManager.slotEquipmentData[i], i);
-            }
-            Transform[] muzzle1 = weaponSlots[0].gun.muzzleFire.gameObject.GetComponentsInChildren<Transform>();
-            Transform[] muzzle2 = weaponSlots[1].gun.muzzleFire.gameObject.GetComponentsInChildren<Transform>();
-            for (int i = 0; i < muzzle1.Length; i++) muzzle1[i].gameObject.layer = LayerMask.NameToLayer("DefaultItem");
-            for (int i = 0; i < muzzle2.Length; i++) muzzle2[i].gameObject.layer = LayerMask.NameToLayer("DefaultItem");
-            weaponSlots[0].gun.muzzleFire.gameObject.layer = LayerMask.NameToLayer("DefaultItem");
-            weaponSlots[1].gun.muzzleFire.gameObject.layer = LayerMask.NameToLayer("DefaultItem");
-            Transform[] list1 = weaponSlots[0].gun.gunVisual.GetComponentsInChildren<Transform>();
-            Transform[] list2 = weaponSlots[1].gun.gunVisual.GetComponentsInChildren<Transform>();
-            Transform[] list3 = weaponSlots[0].gun.attachment.attachmentHolder.GetComponentsInChildren<Transform>();
-            Transform[] list4 = weaponSlots[1].gun.attachment.attachmentHolder.GetComponentsInChildren<Transform>();
-            Debug.Log("Init Start Line 51");
+                InstantiateEquipment(player.playerManager.matchLoadoutManager.slotEquipmentData[i], i);
             EquipItem(0);
-            for (int i = 0; i < list1.Length; i++) list1[i].gameObject.layer = LayerMask.NameToLayer("DefaultItem");
-            for (int i = 0; i < list2.Length; i++) list2[i].gameObject.layer = LayerMask.NameToLayer("DefaultItem");
+            for (int ms = 0; ms < weaponSlots.Length; ms++)
+            {
+                //* Retrieving Weapon parts under each parts' parent
+                Transform[] muzzle1 = weaponSlots[ms].gun.muzzleFire.gameObject.GetComponentsInChildren<Transform>();
+                Transform[] list1 = weaponSlots[ms].gun.gunVisual.GetComponentsInChildren<Transform>();
+                Transform[] list3 = weaponSlots[ms].gun.attachment.attachmentHolder.GetComponentsInChildren<Transform>();
+                Transform[] obj4 = weaponSlots[ms].gun.thirdPersonHandsVisual.GetComponentsInChildren<Transform>();
+                //Transform[] obj2 = weaponSlots[ms].gun.handsVisual.GetComponentsInChildren<Transform>();
 
-            for (int i = 0; i < list3.Length; i++)
-            {
-                list3[i].gameObject.layer = LayerMask.NameToLayer("DefaultItem");
+                //* Setting Weapon parts to be of layer "DefaultItem" so that they don't appear through walls
+                weaponSlots[ms].gun.muzzleFire.gameObject.layer = LayerMask.NameToLayer("DefaultItem");
+                for (int i = 0; i < muzzle1.Length; i++) muzzle1[i].gameObject.layer = LayerMask.NameToLayer("DefaultItem");
+                for (int i = 0; i < list1.Length; i++) list1[i].gameObject.layer = LayerMask.NameToLayer("DefaultItem");
+                for (int i = 0; i < list3.Length; i++) list3[i].gameObject.layer = LayerMask.NameToLayer("DefaultItem");
+                //for (int i = 0; i < obj2.Length; i++) obj2[i].gameObject.layer = LayerMask.NameToLayer("DefaultItem");
+                for (int i = 0; i < obj4.Length; i++) obj4[i].gameObject.layer = LayerMask.NameToLayer("DefaultItem");
+                if (weaponSlots[ms] != null)
+                {
+                    Destroy(weaponSlots[ms].gun.handsVisual);
+                    weaponSlots[ms].gun.transform.localScale = new Vector3(0.7f, 0.7f, 0.7f);
+                }
             }
-            for (int i = 0; i < list4.Length; i++)
+            for (int ms = 0; ms < equipmentSlots.Length; ms++)
             {
-                list4[i].gameObject.layer = LayerMask.NameToLayer("DefaultItem");
-            }
+                //* Retrieving Weapon parts under each parts' parent
+                Transform[] el1 = equipmentSlots[ms].equipment.equipmentVisual.GetComponentsInChildren<Transform>();
+                //Transform[] el3 = equipmentSlots[ms].equipment.handsVisual.GetComponentsInChildren<Transform>();
+                Transform[] el6 = equipmentSlots[ms].equipment.thirdPersonHandsVisual.GetComponentsInChildren<Transform>();
 
-            Transform[] obj1 = weaponSlots[0].gun.handsVisual.GetComponentsInChildren<Transform>();
-            Transform[] obj2 = weaponSlots[1].gun.handsVisual.GetComponentsInChildren<Transform>();
-            for (int i = 0; i < obj1.Length; i++) obj1[i].gameObject.layer = LayerMask.NameToLayer("DefaultItem");
-            for (int i = 0; i < obj2.Length; i++) obj2[i].gameObject.layer = LayerMask.NameToLayer("DefaultItem");
-            Transform[] obj3 = weaponSlots[0].gun.thirdPersonHandsVisual.GetComponentsInChildren<Transform>();
-            Transform[] obj4 = weaponSlots[1].gun.thirdPersonHandsVisual.GetComponentsInChildren<Transform>();
-            for (int i = 0; i < obj3.Length; i++) obj3[i].gameObject.layer = LayerMask.NameToLayer("DefaultItem");
-            for (int i = 0; i < obj4.Length; i++) obj4[i].gameObject.layer = LayerMask.NameToLayer("DefaultItem");
-            if (weaponSlots[0] != null)
-            {
-                Destroy(weaponSlots[0].gun.handsVisual);
-                weaponSlots[0].gun.transform.localScale = new Vector3(0.7f, 0.7f, 0.7f);
-            }
-            if (weaponSlots[1] != null)
-            {
-                Destroy(weaponSlots[1].gun.handsVisual);
-                weaponSlots[1].gun.transform.localScale = new Vector3(0.7f, 0.7f, 0.7f);
-            }
-
-            Transform[] el1 = equipmentSlots[0].equipment.equipmentVisual.GetComponentsInChildren<Transform>();
-            Transform[] el2 = equipmentSlots[1].equipment.equipmentVisual.GetComponentsInChildren<Transform>();
-            for (int i = 0; i < el1.Length; i++) el1[i].gameObject.layer = LayerMask.NameToLayer("DefaultItem");
-            for (int i = 0; i < el2.Length; i++) el2[i].gameObject.layer = LayerMask.NameToLayer("DefaultItem");
-            Transform[] el3 = equipmentSlots[0].equipment.handsVisual.GetComponentsInChildren<Transform>();
-            Transform[] el4 = equipmentSlots[1].equipment.handsVisual.GetComponentsInChildren<Transform>();
-            for (int i = 0; i < el3.Length; i++) el3[i].gameObject.layer = LayerMask.NameToLayer("DefaultItem");
-            for (int i = 0; i < el4.Length; i++) el4[i].gameObject.layer = LayerMask.NameToLayer("DefaultItem");
-            Transform[] el5 = equipmentSlots[0].equipment.thirdPersonHandsVisual.GetComponentsInChildren<Transform>();
-            Transform[] el6 = equipmentSlots[1].equipment.thirdPersonHandsVisual.GetComponentsInChildren<Transform>();
-            for (int i = 0; i < el5.Length; i++) el5[i].gameObject.layer = LayerMask.NameToLayer("DefaultItem");
-            for (int i = 0; i < el6.Length; i++) el6[i].gameObject.layer = LayerMask.NameToLayer("DefaultItem");
-
-            if (equipmentSlots[0] != null)
-            {
-                Destroy(equipmentSlots[0].equipment.handsVisual);
-                equipmentSlots[0].equipment.transform.localScale = new Vector3(0.7f, 0.7f, 0.7f);
-            }
-            if (equipmentSlots[1] != null)
-            {
-                Destroy(equipmentSlots[1].equipment.handsVisual);
-                equipmentSlots[1].equipment.transform.localScale = new Vector3(0.7f, 0.7f, 0.7f);
+                //* Setting Equipment parts to be of layer "DefaultItem" so that they don't appear through walls
+                for (int i = 0; i < el1.Length; i++) el1[i].gameObject.layer = LayerMask.NameToLayer("DefaultItem");
+                //for (int i = 0; i < el3.Length; i++) el3[i].gameObject.layer = LayerMask.NameToLayer("DefaultItem");
+                for (int i = 0; i < el6.Length; i++) el6[i].gameObject.layer = LayerMask.NameToLayer("DefaultItem");
+                if (equipmentSlots[ms] != null)
+                {
+                    Destroy(equipmentSlots[ms].equipment.handsVisual);
+                    equipmentSlots[ms].equipment.transform.localScale = new Vector3(0.7f, 0.7f, 0.7f);
+                }
             }
             transform.position = new Vector3(transform.position.x, transform.position.y + 0.2f, transform.position.z);
         }
