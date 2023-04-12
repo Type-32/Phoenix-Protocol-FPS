b6e9ed297486d638ba7b758957fe9365d8bd6ca3
Type-32 <87076491+Type-32@users.noreply.github.com>
2023-04-13 01:23:06 +0800

    Revert "."
    This reverts commit 4c9381fbcdcf025d540110ee6a17d9dc17e6f2e8.

--- Assets/ShopMenuScript.cs
+++ Assets/ShopMenuScript.cs
@@ -11,119 +11,81 @@ public class ShopMenuScript : MonoBehaviour
     [Header("Menu References")]
     public GameObject weaponsMenu;
     public GameObject cosmeticsMenu;
-
-    [Space]
-    [Header("Selections")]
-    public GameObject weaponsMenuSelection;
-    public GameObject cosmeticsMenuSelection;
+    public GameObject lootCratesMenu;
 
     [Space, Header("Weapons Menu Refs")]
     public GameObject shopWeaponItemPrefab;
     public Transform shopWeaponItemHolder;
-    public GameObject shopSelectedWeaponPreview;
 
     [Space, Header("Preview UI Refs")]
     public GameObject previewUI;
-    public GameObject previewAttachItemPrefab;
-    public Transform previewAttachItemHolder;
-    public Image previewIcon;
-    public Text previewWeaponName;
-    public Text previewWeaponPrice;
-    public Slider previewDamage;
-    public Slider previewFireRate;
-    public Slider previewAmmo;
-    public Slider previewRange;
-    public Button purchasePreview;
+    [SerializeField] private GameObject previewAttachItemPrefab;
+    [SerializeField] private Transform previewAttachItemHolder;
+    [SerializeField] private Image previewIcon;
+    [SerializeField] private Text previewWeaponName;
+    [SerializeField] private Text previewWeaponPrice;
+    [SerializeField] private LoadoutWeaponStatisticsDisplay previewStats;
+    [SerializeField] List<LAPreview> attachList = new();
+    [SerializeField] List<ShopWeaponHoriItem> shopWeaponList = new();
+    [HideInInspector] public WeaponData CurrentPreviewWeaponData;
 
     [Space, Header("Weapon Menu")]
-    [SerializeField] WeaponData previewingWeaponData;
-    [SerializeField] List<ShopAttachPreviewItem> attachList = new();
-    [SerializeField] List<ShopWeaponItem> shopWeaponList = new();
 
     [Space, Header("Cosmetics Menu")]
     [SerializeField] GameObject weaponCosmeticListItem;
     [SerializeField] Transform weaponCosmeticListItemHolder;
-    [SerializeField] List<WeaponCosmeticListItem> weaponCosmeticList;
+    [SerializeField] Dictionary<WeaponData, WeaponCosmeticListItem> weaponCosmeticList = new();
 
     void Start()
     {
-<<<<<<< HEAD
         InitializeWeaponsMenu();
         //InitializeCosmeticsMenu();
         weaponsMenu.SetActive(true);
         //cosmeticsMenu.SetActive(false);
-=======
-        UserDataJSON jsonData = FileOps<UserDataJSON>.ReadFile(UserSystem.UserDataPath);
-        InitializeWeaponsMenu(jsonData);
-        InitializeCosmeticsMenu(jsonData.AppearancesData);
-        weaponsMenu.SetActive(false);
-        cosmeticsMenu.SetActive(false);
->>>>>>> parent of 962acc21 (Overhauling Shop Menu)
         TogglePreviewUI(false);
-        MenuManager.Instance.ToggleShopMenu(false);
+        //MenuManager.Instance.ToggleShopMenu(false);
     }
 
-    private void InitializeCosmeticsMenu(AppearancesDataJSON jsonData)
+    private void InitializeCosmeticsMenu()
     {
-<<<<<<< HEAD
         AppearancesDataJSON jsonData = FileOps<UserDataJSON>.ReadFile(UserSystem.UserDataPath).AppearancesData;
-=======
-        return;
->>>>>>> parent of 962acc21 (Overhauling Shop Menu)
         for (int i = 0; i < jsonData.availableWeaponAppearances.Count; i++)
         {
+            var apdata = CosmeticSystem.FindWeaponAppearanceMeshData(jsonData.availableWeaponAppearances[i]);
             WeaponCosmeticListItem temp = Instantiate(weaponCosmeticListItem, weaponCosmeticListItemHolder).GetComponent<WeaponCosmeticListItem>();
-            weaponCosmeticList.Add(temp);
-            temp.SetInfo(CosmeticSystem.FindWeaponAppearanceMeshData(jsonData.availableWeaponAppearances[i]), this);
+            weaponCosmeticList.Add(apdata.weaponData, temp);
+            temp.SetInfo(apdata, this);
+            temp.gameObject.SetActive(false);
         }
     }
-
-    private void InitializeWeaponsMenu(UserDataJSON jsonData)
+    private void InitializeWeaponsMenu()
     {
+        UserDataJSON jsonData = FileOps<UserDataJSON>.ReadFile(UserSystem.UserDataPath);
         bool informPopupNeeded = false;
         string content = "You have unlocked the following content(s) in your last session: \n";
         for (int i = 0; i < GlobalDatabase.Instance.allWeaponDatas.Count; i++)
         {
-<<<<<<< HEAD
             ShopWeaponHoriItem item = Instantiate(shopWeaponItemPrefab, shopWeaponItemHolder).GetComponent<ShopWeaponHoriItem>();
             item.ToggleSelection(false);
-=======
-            ShopWeaponItem item = Instantiate(shopWeaponItemPrefab, shopWeaponItemHolder).GetComponent<ShopWeaponItem>();
-            bool un = false, pur = false;
->>>>>>> parent of 962acc21 (Overhauling Shop Menu)
             if (jsonData.ShopData.availableWeaponIndexes.Contains(i) || jsonData.ShopData.unlockedWeaponIndexes.Contains(i) || jsonData.ShopData.ownedWeaponIndexes.Contains(i))
             {
                 if (jsonData.ShopData.availableWeaponIndexes.Contains(i))
                 {
-                    un = pur = false;
                     if (jsonData.userLevel >= GlobalDatabase.Instance.allWeaponDatas[i].unlockingLevel)
                     {
-                        un = true;
-                        pur = false;
                         informPopupNeeded = true;
                         jsonData.ShopData.availableWeaponIndexes.Remove(i);
                         jsonData.ShopData.unlockedWeaponIndexes.Add(i);
                         content = content + GlobalDatabase.Instance.allWeaponDatas[i].itemName + "\n";
                     }
                 }
-                if (jsonData.ShopData.unlockedWeaponIndexes.Contains(i))
-                {
-                    un = true;
-                    pur = false;
-                }
-                if (jsonData.ShopData.ownedWeaponIndexes.Contains(i))
-                {
-                    un = pur = true;
-                }
-                item.SetItemData(GlobalDatabase.Instance.allWeaponDatas[i], un, pur, this);
+                item.SetInfo(GlobalDatabase.Instance.allWeaponDatas[i], this);
                 shopWeaponList.Add(item);
             }
             else
             {
                 if (jsonData.userLevel >= GlobalDatabase.Instance.allWeaponDatas[i].unlockingLevel)
                 {
-                    un = true;
-                    pur = false;
                     informPopupNeeded = true;
                     jsonData.ShopData.unlockedWeaponIndexes.Add(i);
                     content = content + GlobalDatabase.Instance.allWeaponDatas[i].itemName + "\n";
@@ -132,32 +94,25 @@ public class ShopMenuScript : MonoBehaviour
                 {
                     jsonData.ShopData.availableWeaponIndexes.Add(i);
                 }
-                item.SetItemData(GlobalDatabase.Instance.allWeaponDatas[i], un, pur, this);
+                item.SetInfo(GlobalDatabase.Instance.allWeaponDatas[i], this);
                 shopWeaponList.Add(item);
             }
         }
         FileOps<UserDataJSON>.WriteFile(jsonData, UserSystem.UserDataPath);
         if (informPopupNeeded) MenuManager.Instance.AddModalWindow("Unlocking Content", content);
     }
-    public void SetPreviewInfo(WeaponData data, bool showPurchaseButton)
+    public void SetPreviewInfo(WeaponData data)
     {
-<<<<<<< HEAD
         foreach (ShopWeaponHoriItem i in shopWeaponList)
         {
             if (i.CachedWeaponData == data) continue;
             i.ToggleSelection(false);
         }
         CurrentPreviewWeaponData = data;
-=======
-        previewingWeaponData = data;
->>>>>>> parent of 962acc21 (Overhauling Shop Menu)
         previewIcon.sprite = data.itemIcon;
         previewWeaponName.text = data.itemName;
-        previewWeaponPrice.text = "$" + data.purchasePrice.ToString();
-        previewDamage.value = data.damage;
-        previewFireRate.value = data.fireRate;
-        previewRange.value = data.range;
-        previewAmmo.value = data.maxAmmoPerMag;
+        previewWeaponPrice.text = $"${data.purchasePrice}";
+        previewStats.SetInfo(data.damage, data.range, data.maxAmmoPerMag);
         if (attachList.Count != 0)
         {
             for (int i = 0; i < attachList.Count; i++)
@@ -168,34 +123,9 @@ public class ShopMenuScript : MonoBehaviour
         }
         for (int i = 0; i < data.applicableAttachments.Count; i++)
         {
-            ShopAttachPreviewItem item = Instantiate(previewAttachItemPrefab, previewAttachItemHolder).GetComponent<ShopAttachPreviewItem>();
+            LAPreview item = Instantiate(previewAttachItemPrefab, previewAttachItemHolder).GetComponent<LAPreview>();
             attachList.Add(item);
-            item.SetInfo(data.applicableAttachments[i].attachmentIcon, this);
-        }
-        purchasePreview.interactable = showPurchaseButton;
-    }
-    public void ToggleWeaponsMenu(bool value)
-    {
-        weaponsMenu.SetActive(value);
-        if (value)
-        {
-            cosmeticsMenu.SetActive(false);
-            cosmeticsMenuSelection.SetActive(false);
-            weaponsMenuSelection.SetActive(true);
-        }
-        else
-        {
-            TogglePreviewUI(false);
-        }
-    }
-    public void ToggleCratesMenu(bool value)
-    {
-        cosmeticsMenu.SetActive(value);
-        if (value)
-        {
-            weaponsMenu.SetActive(false);
-            weaponsMenuSelection.SetActive(false);
-            cosmeticsMenuSelection.SetActive(true);
+            item.SetInfo(data.applicableAttachments[i]);
         }
     }
     public void TogglePreviewUI(bool value)
@@ -204,25 +134,20 @@ public class ShopMenuScript : MonoBehaviour
     }
     public void OnClickPurchaseButton()
     {
-<<<<<<< HEAD
         foreach (ShopWeaponHoriItem i in shopWeaponList)
         {
             i.ToggleSelection(false);
         }
         PurchaseWeapon(CurrentPreviewWeaponData);
         TogglePreviewUI(false);
-=======
-        PurchaseWeapon(previewingWeaponData);
->>>>>>> parent of 962acc21 (Overhauling Shop Menu)
     }
     public void PurchaseWeapon(WeaponData data)
     {
         if (data != null)
         {
             UserDataJSON jsonData = FileOps<UserDataJSON>.ReadFile(UserSystem.UserDataPath);
-            if (jsonData.userCoins >= data.purchasePrice)
+            if (jsonData.userCoins >= data.purchasePrice && jsonData.userLevel >= data.unlockingLevel)
             {
-<<<<<<< HEAD
                 if (jsonData.ShopData.ownedWeaponIndexes.Contains(data.GlobalWeaponIndex))
                 {
                     MenuManager.Instance.AddModalWindow("Error", "You have already purchased the weapon " + data.itemName + ".");
@@ -230,37 +155,37 @@ public class ShopMenuScript : MonoBehaviour
                 }
                 MenuManager.Instance.AddModalWindow("Purchase Result", "You have purchased the weapon " + data.itemName + " successfully!\nYou can equip this weapon in your loadouts now.");
                 //FindForWeaponDataInList(data).SetInfo(data, true, true, this);
-=======
-                MenuManager.Instance.AddNotification("Purchase Result", "You have purchased the weapon " + data.itemName + " successfully!\nYou can equip this weapon in your loadouts now.");
-                FindForWeaponDataInList(data).SetItemData(data, true, true, this);
->>>>>>> parent of 962acc21 (Overhauling Shop Menu)
                 UserDatabase.Instance.AddUserCurrency(data.purchasePrice);
                 jsonData.ShopData.ownedWeaponIndexes.Add(Database.FindWeaponDataIndex(data));
-                purchasePreview.interactable = false;
-
                 FileOps<UserDataJSON>.WriteFile(jsonData, UserSystem.UserDataPath);
                 MenuManager.Instance.loadoutSelectionMenu.GetComponent<LoadoutSelectionScript>().InstantiateLoadoutItemSelections();
             }
+            else if (jsonData.userCoins < data.purchasePrice && jsonData.userLevel >= data.unlockingLevel)
+            {
+                MenuManager.Instance.AddModalWindow("Error", "Cannot Purchase Weapon! You need more money!");
+                TogglePreviewUI(false);
+            }
+            else if (jsonData.userCoins >= data.purchasePrice && jsonData.userLevel < data.unlockingLevel)
+            {
+                MenuManager.Instance.AddModalWindow("Error", "Cannot Purchase Weapon! You need more levels!");
+                TogglePreviewUI(false);
+            }
             else
             {
-                MenuManager.Instance.AddNotification("Purchase Result", "Cannot Purchase Weapon! You need more money!");
+                MenuManager.Instance.AddNotification("Error", "Cannot Purchase Weapon! You need more money and levels!");
+                TogglePreviewUI(false);
             }
         }
     }
-    public ShopWeaponItem FindForWeaponDataInList(WeaponData data)
+    public ShopWeaponHoriItem FindForWeaponDataInList(WeaponData data)
     {
         for (int i = 0; i < shopWeaponList.Count; i++)
         {
-            if (shopWeaponList[i].weaponData == data)
+            if (shopWeaponList[i].CachedWeaponData == data)
             {
                 return shopWeaponList[i];
             }
         }
         return new();
-    }
-    public void RemoveWeaponCosmeticListItem(WeaponCosmeticListItem item)
-    {
-        if (weaponCosmeticList.Contains(item)) weaponCosmeticList.Remove(item);
-        Destroy(item.gameObject);
     }
 }
